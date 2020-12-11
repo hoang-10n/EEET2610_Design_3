6387eafb7c2980973811c7aff637b2f071d9d427
s3740819 <56997194+s3740819@users.noreply.github.com>
2020-12-11 12:48:34 +0700

    Update blackObjectDetection.py

--- blackObjectDetection.py
+++ blackObjectDetection.py
@@ -1,21 +1,17 @@
 import cv2
 import numpy as np
-import imutils
-
-
-
+GRAY = 255
 cap = cv2.VideoCapture(0)
 while True:
-    _, image = cap.read()
+    _, live = cap.read()
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = 255 - gray
-    ret, thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)
+    black = GRAY - cv2.cvtColor(live, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(black, 225, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) >= 150:        # Change 150 to 300 when demo (i will explain this at school)
-        image = cv2.drawContours(image, contours, -1, (0, 0, 255), 3)
+    if len(contours) >= 150:  # Change 150 to 300 when demo (i will explain this at school)
+        live = cv2.drawContours(live, contours, -1, (0, 0, 255), 3)
         print("black!!!")
-    cv2.imshow('frame', image)
+    cv2.imshow('frame', live)
     if cv2.waitKey(1) == ord("q"):
         break
 
